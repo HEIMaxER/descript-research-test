d925efbc4af1cfc280bf1c15b55386af9f5a2a7f
HEIMaxER <maxime.plocharski@gmail.com>
2020-01-31 16:22:11 +0100

    added dummy methode

--- models/dummy.py
+++ models/dummy.py
@@ -1,15 +1,53 @@
-import numpy
-strokes = numpy.load('../data/strokes-py3.npy', allow_pickle=True)
-stroke = strokes[0]
+from keras.models import load_model
+from utils import plot_stroke
+import random as rd
+import numpy as np
+
+seeds = np.load('../data/X_test.npy', allow_pickle=True)
+unconditional_model = load_model('../models/unconditional_generation_model_double_output_best.h5')
 
 
 def generate_unconditionally(random_seed=1):
-    # Input:
-    #   random_seed - integer
 
-    # Output:
-    #   stroke - numpy 2D-array (T x 3)
-    return stroke
+    gen_len = 400 + rd.randint(0, 50)
+    gen_stroke = []
+    
+    gen_buffer = seeds[random_seed]
+    
+    start_gen = 0
+    i = 0
+    
+    while start_gen == 0 and i <1000:
+        new_step = unconditional_model.predict(np.array([gen_buffer]))
+        if rd.random() <= p :
+            new_step[0][0][0] = 1
+            start_gen = 1
+        else :
+            new_step[0][0][0] = 0
+        new_step = np.array([new_step[0][0][0], new_step[1][0][0], new_step[1][0][1]])
+        new_step = np.reshape(new_step, newshape=(1, 3))
+        gen_buffer = gen_buffer[1:]
+        gen_buffer = np.append(gen_buffer, new_step, axis=0)
+        i+=1
+    
+    stop_gen = 0
+    i=0
+    while i <gen_len:
+        new_step = unconditional_model.predict(np.array([gen_buffer]))
+        if rd.random() <= p :
+            new_step[0][0][0] = 1
+            if i >= gen_len:
+                stop_gen = 1
+        else :
+            new_step[0][0][0] = 0
+        new_step = np.array([new_step[0][0][0], new_step[1][0][0], new_step[1][0][1]])
+        new_step = np.reshape(new_step, newshape=(1, 3))
+        gen_buffer = gen_buffer[1:]
+        gen_buffer = np.append(gen_buffer, new_step, axis=0)
+        gen_stroke.append(new_step[0])
+        i+=1
+    gen_stroke = np.array(gen_stroke)
+    return gen_stroke
 
 
 def generate_conditionally(text='welcome to lyrebird', random_seed=1):
